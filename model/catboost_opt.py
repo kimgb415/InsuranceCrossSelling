ddfac0bf61c4d40dce30a8c87b422d479c04b0ac
gakim <gakim@ea.com>
2024-07-18 13:16:26 +0900

    second round of catboost tuning
    - 1045 mins for 40 rounds of 3-fold cross validation
    - min logloss of 0.25
    - speed up settings for catboost training

--- model/catboost_opt.py
+++ model/catboost_opt.py
@@ -11,25 +11,42 @@ def catboost_optuna_objective(trial : optuna.Trial, dataset: cb.Pool, fixed_para
         pool=dataset,
         params={
             **fixed_params,
-            'learning_rate': trial.suggest_float('learning_rate', 0.0001, 0.1, log=True),
-            'depth': trial.suggest_int('depth', 4, 10),
+            'learning_rate': trial.suggest_float('learning_rate', 0.001, 0.1, log=True),
+            # Maximum tree depth is 16
+            'depth': trial.suggest_int('depth', 6, 16),
             'l2_leaf_reg': trial.suggest_float('l2_leaf_reg', 0.0001, 0.1, log=True),
-            'bagging_temperature': trial.suggest_float('bagging_temperature', 0, 1),
-            'random_strength': trial.suggest_int('random_strength', 0, 15),
-            'border_count': trial.suggest_int('border_count', 32, 255),
+            'bagging_temperature': trial.suggest_float('bagging_temperature', 0.75, 1),
+            # for imbalanced datasets
+            'auto_class_weights': trial.suggest_categorical('auto_class_weights', ['None', 'Balanced', 'SqrtBalanced']),
+            # 'iterations' (Aliases: num_boost_round, n_estimators, num_trees)
+            'n_estimators': trial.suggest_int('n_estimators', 100, 1000),
+            'early_stopping_rounds': 50,
+            # Try setting leaf_estimation_iterations to 1 or 5 to speed up the training on datasets with a small number of features.
+            'leaf_estimation_iterations': trial.suggest_categorical('leaf_estimation_iterations', [1, 5]),
         },
-        fold_count=5,
+        fold_count=3,
         partition_random_seed=RANDOM_SEED,
         verbose=False,
     )
 
-    return np.max(scores['test-AUC-mean'])
+    # return np.max(scores['test-AUC-mean'])
+    return np.min(scores['test-Logloss-mean'])
 
 const_params = {
     'task_type': 'GPU',
-    'loss_function': 'CrossEntropy',
-    'eval_metric': 'AUC', 
-    'custom_metric': ['AUC'],
-    'iterations': 100,
-    'random_seed': RANDOM_SEED
+    'loss_function': 'Logloss',
+    'eval_metric': 'Logloss', 
+    'custom_metric': ['Logloss'],
+    'random_seed': RANDOM_SEED,
+    # fixed after tuning
+    'random_strength': 0,
+
+    # --------------- Speed Up Training -------------
+    'bootstrap_type': 'Bayesian',
+    # Ordered — Usually provides better quality on small datasets, but it may be slower than the Plain scheme.
+    # Plain — The classic gradient boosting scheme.
+    'boosting_type': 'Plain',
+    # Try to set border_count of this parameter to 32 if training is performed on GPU. 
+    # In many cases, this does not affect the quality of the model but significantly speeds up the training.
+    'border_count': 32,
 }
