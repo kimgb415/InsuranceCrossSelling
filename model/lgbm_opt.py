b6b0504ddfb45571ff298cca98f753553287cad3
gakim <gakim@ea.com>
2024-07-23 22:00:49 +0900

    lgbm case study 2 best AUC 0.886

--- model/lgbm_opt.py
+++ model/lgbm_opt.py
@@ -4,12 +4,12 @@ import numpy as np
 import pandas as pd
 
 RANDOM_STATE = 1
+MAX_BIN_CHOICE = [2 ** i - 1 for i in [15, 18, 20, 22]]
 
 # https://lightgbm.readthedocs.io/en/stable/Parameters-Tuning.html
 lgbm_fixed_params = {
     'n_estimators': 500,
     'eval_metric': 'auc',
-    'max_bin': 32767,
     'random_state': RANDOM_STATE,
     'early_stopping_rounds': 50,
     'verbosity': -1,
@@ -17,17 +17,24 @@ lgbm_fixed_params = {
     # https://lightgbm.readthedocs.io/en/latest/GPU-Tutorial.html#gpu-setup
     # "device" : "gpu",
 
+    # Fixed after first tuning
+    'colsample_bytree': 0.25,
+    'max_depth': 12,
+    'num_leaves': 220,
+
     # Use less data for faster tuning
     'bagging_freq': 5,
     'bagging_fraction': 0.75,
 }
 
 def optuna_objective(trial : optuna.Trial, fixed_params, x_train : pd.DataFrame, y_train, x_test, y_test):
+    scale_pos_weight = np.sum(y_train == 0) / np.sum(y_train == 1)
     tunable_params = {
-        'learning_rate': trial.suggest_float('learning_rate', 0.005, 0.1),
-        'colsample_bytree': trial.suggest_float('colsample_bytree', 0.1, 0.75),
-        'num_leaves': trial.suggest_int('num_leaves', 64, 256),
-        'max_depth': trial.suggest_int('max_depth', 5, 15),
+        'learning_rate': trial.suggest_float('learning_rate', 0.05, 0.1),
+        'reg_lambda': trial.suggest_float('reg_lambda', 0.001, 0.5),
+        'boosting_type': trial.suggest_categorical('boosting_type', ['gbdt', 'dart', 'rf']),
+        'scale_pos_weight': trial.suggest_categorical('scale_pos_weight', [1, scale_pos_weight]),
+        'max_bin': trial.suggest_categorical('max_bin', MAX_BIN_CHOICE)
     }
 
     cat_features = list(x_train.select_dtypes(include=['object']).columns)
