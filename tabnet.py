fa4d57276a3d4a7efcf5863ac1d127ab1c57c365
gabhyun kim <kimgb415@gmail.com>
2024-07-29 09:40:26 +0900

    tabnet first tuning

--- tabnet.py
+++ tabnet.py
@@ -8,25 +8,76 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import OrdinalEncoder
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = f"1"
+import optuna
+from pprint import pprint
+
+
+SEED = 1
 
 tabnet_params = {
     "optimizer_fn":torch.optim.Adam,
-    "optimizer_params":dict(lr=2e-2),
+    "optimizer_params":dict(lr=1e-1),
     "scheduler_params":{
-        "step_size":50, # how to use learning rate scheduler
-        "gamma":0.9
+        "step_size":10,
+        "gamma":0.5
     },
     "scheduler_fn":torch.optim.lr_scheduler.StepLR,
     # "mask_type":'entmax', # "sparsemax"
+    'device_name':'cuda',
+    'seed':SEED,
 }
 
-def tabnet_date_preprocess(df: pd.DataFrame):
+
+def tabnet_data_preprocess(df: pd.DataFrame):
     for col in df.columns:
         if df[col].dtype == 'object':
             df[col] = OrdinalEncoder().fit_transform(df[col].values.reshape(-1, 1))
     
     return df
+
+
+def objective(trial : optuna.Trial, X_train, y_train, X_test, y_test):
+    cat_idx = [i for i, col in enumerate(X_train.columns) if col in X_train.select_dtypes(include=['object']).columns]
+    # https://github.com/dreamquark-ai/tabnet/issues/357#issuecomment-1030270567
+    # train_test_split might not cover all unique categories in each categorical column
+    X_merged = pd.concat([X_train, X_test])
+    cat_dims = [len(X_merged[col].unique()) for col in cat_idx]
+    emb_dims = [min(20, (x + 1) // 2) for x in cat_dims]
+
+    params = {
+        **tabnet_params,
+        'n_d': trial.suggest_int('n_d', 8, 64),
+        'n_steps': trial.suggest_int('n_steps', 3, 20),
+        'gamma': trial.suggest_float('gamma', 1.0, 2.0),
+        'n_independent': trial.suggest_int('n_independent', 1, 5),
+        'n_shared': trial.suggest_int('n_shared', 1, 5)
+    }
+
+    # According to the paper n_d=n_a is usually a good choice.
+    params['n_a'] = params['n_d']
+
+    model = TabNetClassifier(
+        **params,
+        cat_idxs=cat_idx,
+        cat_dims=cat_dims,
+        cat_emb_dim=emb_dims,
+    )
+
+    model.fit(
+        X_train=X_train.values, y_train=y_train.values, 
+        eval_set=[(X_test.values, y_test.values)], eval_name=['valid'], eval_metric=['auc'], 
+        max_epochs=3, 
+        patience=50, 
+        batch_size=trial.suggest_categorical('batch_size', [1024, 2048, 4096]), 
+        virtual_batch_size=128, 
+        drop_last=False,
+        # feature importance computing is extremely slow
+        compute_importance=False,
+    )
+
+    pprint(model.history)
+
+    return max(model.history['valid_auc'])
 
 
 def tabnet_inference(X_train: pd.DataFrame, X_test, y_train, y_test):
@@ -54,15 +105,14 @@ def tabnet_inference(X_train: pd.DataFrame, X_test, y_train, y_test):
         num_workers=0, 
         drop_last=False,
     )
-
-    plt.plot(model.history['valid_auc'])
+    model.save_model('tabnet_model')
 
     pass
 
 def main(args):
     X_train, X_test, y_train, y_test = generate_train_test_data()
-    X_train = tabnet_date_preprocess(X_train)
-    X_test = tabnet_date_preprocess(X_test)
+    X_train = tabnet_data_preprocess(X_train)
+    X_test = tabnet_data_preprocess(X_test)
     if args.tune:
         pass
     elif args.inference:
